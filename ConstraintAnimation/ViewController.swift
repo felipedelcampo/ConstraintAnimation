f6a79273c413b3df39dd6305d994af28a5e41e26
Felipe Del Campo Gomes dos Santos <felipegs@ciandt.com>
2017-12-19 07:47:15 -0200

    Initial Commit

---
+++ ConstraintAnimation/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ConstraintAnimation
+//
+//  Created by Felipe Del Campo Gomes dos Santos on 19/12/2017.
+//  Copyright © 2017 Felipe Del Campo Gomes dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func animateButtonTouched(_ sender: UIButton) {
+        
+        // TODO: animate here
+        
+    }
+    
+}
+
